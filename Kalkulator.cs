172e42ab81e058b51c018363414933dd0aed0e18
Dion4286 <101573639+Dion4286@users.noreply.github.com>
2022-03-17 18:07:25 +0700

    Tugas Praktikum Pemrogaman 2
    Nama : Dionisius Rizky Pramusinto
    NIM : 21.11.4286
    Kelas : 21-IF-07
    Nama Praktikum : Membuat Kalkulator

---
+++ Kalkulator.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Calculator
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Kalkulator");
+
+        int angka1;
+        int angka2;
+        int pil;
+        int total;
+
+        Console.WriteLine("1. Penjumlahan");
+        Console.WriteLine("2. Pengurangan");
+        Console.WriteLine("3. Perkalian");
+        Console.WriteLine("4. Pembagian");
+
+        Console.WriteLine("Pilih menu kalkulator : ");
+        pil = int.Parse(Console.ReadLine());
+
+        if (pil ==1)
+        {
+            Console.WriteLine("Masukkan angka pertama :");
+            angka1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukkan angka kedua :");
+            angka2 = Convert.ToInt32(Console.ReadLine());
+
+            total = angka1 + angka2;
+            Console.WriteLine("Hasil : " + total);
+        }
+        else if (pil == 2)
+        {
+            Console.WriteLine("Masukkan angka pertama :");
+            angka1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukkan angka kedua :");
+            angka2 = Convert.ToInt32(Console.ReadLine());
+
+            total = angka1 - angka2;
+            Console.WriteLine("Hasil : " + total);
+        }
+        else if (pil == 3)
+        {
+            Console.WriteLine("Masukkan angka pertama :");
+            angka1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukkan angka kedua :");
+            angka2 = Convert.ToInt32(Console.ReadLine());
+
+            total = angka1 * angka2;
+            Console.WriteLine("Hasil : " + total);
+        }
+        else if (pil == 4)
+        {
+            Console.WriteLine("Masukkan angka pertama :");
+            angka1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukkan angka kedua :");
+            angka2 = Convert.ToInt32(Console.ReadLine());
+
+            total = angka1 / angka2;
+            Console.WriteLine("Hasil : " + total);
+        }
+        else if (pil == 5)
+        {
+            Console.WriteLine("Maaf, menu yang Anda pilih tidak tersedia");
+        }
+        Console.WriteLine("\nTekan sembarang key untuk keluar");
+        Console.ReadKey();
+    }
+}
